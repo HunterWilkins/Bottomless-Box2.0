9b66eb0e64c03d824835f315d917281d75d6b223
HunterWilkins <wilkins.hunter@gmail.com>
2019-10-19 14:31:21 -0500

    Added new left arrow for mobile

--- client/src/pages/Info/index.js
+++ client/src/pages/Info/index.js
@@ -74,7 +74,7 @@ class Home extends Component {
         return (
             <div>
                 <nav>
-                    <Link to = "/" id = "homelink">⭠</Link>
+                    <Link to = "/" id = "homelink">🠈</Link>
                     <h1 id = "info-title">Info</h1>
                 </nav>
 
