c5f5758736a0b97b5df58f73cf0496dfd246755d
HunterWilkins <wilkins.hunter@gmail.com>
2019-10-19 14:39:57 -0500

    Left Arrow Attempt #4

--- client/src/pages/Info/index.js
+++ client/src/pages/Info/index.js
@@ -74,7 +74,7 @@ class Home extends Component {
         return (
             <div>
                 <nav>
-                    <Link to = "/" id = "homelink">&#x2190</Link>
+                    <Link to = "/" id = "homelink">	◀</Link>
                     <h1 id = "info-title">Info</h1>
                 </nav>
 
